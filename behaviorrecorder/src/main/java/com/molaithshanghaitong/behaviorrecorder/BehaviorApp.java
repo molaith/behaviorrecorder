0ffcfeadf875c894826fa418b7e6d001e912a7e4
molaith <molaith@gmail.com>
2016-12-15 15:53:14 +0800

    create userbehaviorrecordlib

---
+++ behaviorrecorder/src/main/java/com/molaithshanghaitong/behaviorrecorder/BehaviorApp.java
@@ -0,0 +1,14 @@
+package com.molaithshanghaitong.behaviorrecorder;
+
+import android.app.Application;
+
+/**
+ * Created by daiji on 2016/12/15.
+ */
+public class BehaviorApp extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ behaviorrecorder/src/main/java/com/molaithshanghaitong/behaviorrecorder/config/BehaviorServiceConfig.java
@@ -0,0 +1,10 @@
+package com.molaithshanghaitong.behaviorrecorder.config;
+
+/**
+ * Created by daiji on 2016/12/15.
+ */
+public class BehaviorServiceConfig {
+
+    
+
+}
